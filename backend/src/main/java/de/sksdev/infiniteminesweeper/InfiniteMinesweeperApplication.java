e33a4561eee872e325252a8ae1e32f19e2b92c92
Samuel Klein <it@samuel-klein.net>
2020-03-22 18:36:27 +0100

    initial commit

---
+++ backend/src/main/java/de/sksdev/infiniteminesweeper/InfiniteMinesweeperApplication.java
@@ -0,0 +1,13 @@
+package de.sksdev.infiniteminesweeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfiniteMinesweeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfiniteMinesweeperApplication.class, args);
+    }
+
+}
