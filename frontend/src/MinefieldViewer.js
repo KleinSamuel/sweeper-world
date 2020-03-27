0053bfd08c4e9c0428cac5ae2b832681375e0383
Samuel Klein <it@samuel-klein.net>
2020-03-27 23:53:25 +0100

    added basic routes for updates, added coordinates to ui

--- frontend/src/MinefieldViewer.js
+++ frontend/src/MinefieldViewer.js
@@ -7,13 +7,10 @@ import Cursor from "./Cursor";
 import UserInterface from "./UserInterface";
 import MinefieldModel from "./MinefieldModel";
 import Communicator from "./Communicator";
-import { Howl } from "howler";
 
 export default class MinefieldViewer {
 
     constructor() {
-
-        console.log("web gl? "+PIXI.utils.isWebGLSupported());
 
         this.GLOBAL_POS_X = 0;
         this.GLOBAL_POS_Y = 0;
@@ -57,7 +54,7 @@ export default class MinefieldViewer {
             context.app = app;
             context.app.renderer.resize(window.innerWidth, window.innerHeight);
 
-            context.minefieldModel = new MinefieldModel(context.com, context.GLOBAL_POS_X, context.GLOBAL_POS_Y);
+            context.minefieldModel = new MinefieldModel(context, context.com, context.GLOBAL_POS_X, context.GLOBAL_POS_Y);
             context.minefieldModel.init().then(resolve);
         });
     }
@@ -74,8 +71,8 @@ export default class MinefieldViewer {
         context.app.stage.addChildAt(context.minefieldModel, 0);
         context.app.stage.addChildAt(context.cursor, 1);
         context.app.stage.addChildAt(context.ui, 2);
-        context.app.stage.addChildAt(context.startscreen, 3);
 
+        /*
         const explosionTextures = [];
         let i;
 
@@ -94,16 +91,14 @@ export default class MinefieldViewer {
         explosion.loop = false;
         context.app.stage.addChild(explosion);
         context.exp = explosion;
-
-        context.exp_sound = new Howl({
-            src: "assets/explosion.mp3"
-        });
+        */
 
         window.addEventListener("resize", function(){
             context.app.renderer.resize(window.innerWidth, window.innerHeight);
             context.ui.resize(window.innerWidth, window.innerHeight);
         });
 
+        // helper variables for the tracking if the field is being dragged
         let fieldX = -1;
         let fieldY = -1;
         let mouseDownX = -1;
@@ -117,31 +112,11 @@ export default class MinefieldViewer {
         });
 
         window.addEventListener("mouseup", function(event){
-
             context.cursor.sprite.visible = true;
-
             mouseDownX = -1;
             mouseDownY = -1;
             fieldX = -1;
             fieldY = -1;
-            /*
-            event.button = 3;
-            if (event.button === 0) {
-                let code = context.minefieldModel.clickCell(chunkX, chunkY, cellX, cellY);
-                context.updateField();
-                if (code === 1) {
-                    context.exp.visible = true;
-                    context.exp.animationSpeed = 0.5;
-                    context.exp.x = context.cursor.x + CONFIG.CELL_PIXEL_SIZE/2;
-                    context.exp.y = context.cursor.y + CONFIG.CELL_PIXEL_SIZE/2;
-                    context.exp.gotoAndPlay(1);
-                    context.exp_sound.play();
-                }
-            } else if (event.button === 2) {
-                context.minefieldModel.flagCell(chunkX, chunkY, cellX, cellY);
-                context.updateField();
-            }
-            */
         });
 
         window.addEventListener("mousemove", function(event){
